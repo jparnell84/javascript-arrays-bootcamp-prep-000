91ecdcdc0471f2b56440e5e959adf2bee4d2543a
Jonathan Parnell <jonathan.parnell1@gmail.com>
2020-08-24 22:56:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,35 +1,30 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(){
-  return ["foo",...[1]];
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens;
 }
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift("foo");
-  return array;
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens;
 }
-function addElementToEndOfArray(){
-  return [...[1],"foo"];
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
 }
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push("foo");
-  return array;
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens;
 }
-function accessElementInArray(){
-  var numbers = [1,2,3];
-  return numbers[2];
+function appendKitten(name){
+  return [...kittens, "Broom"];
 }
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
-  return array;
+function prependKitten(name){
+  return ["Arnold",...kittens];
 }
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+function removeLastKitten(name){
+  return kittens.slice(0, kittens.length - 1);
 }
-function removeElementFromEndOfArray(){
-  var numbers = [1,2,3];
-  return numbers.slice(0, numbers.length-1);
-}
-function removeElementFromBeginningOfArray(array){
-  var numbers = [1,2,3];
-  return numbers.slice(-2);
+function removeFirstKitten(name){
+  return kittens.slice(1);
 }
